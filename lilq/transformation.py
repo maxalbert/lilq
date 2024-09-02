08f3966392db16a61de5afacc5390f9120e3c95d
Maximilian Albert <maximilian.albert@gmail.com>
2024-09-02 08:10:55 +0100

    Namedtuples are transformed correctly

--- lilq/transformation.py
+++ lilq/transformation.py
@@ -1,9 +1,21 @@
+def get_transformer_for(item):
+    if isinstance(item, tuple) and hasattr(item, "_replace"):
+        return lambda item, rules: item._replace(**rules)
+    elif hasattr(item, "__dict__"):
+        return lambda item, rules: item.__dict__.update(**rules)
+    else:
+        raise NotImplementedError(f"No transformer available for item: {item} (type: {type(item)})")
+
+
 class Transformation:
     def __init__(self, *, query, rules):
         self.query = query
         self.rules = rules
 
     def apply(self, item):
-        if self.query.matches(item):
-            item.__dict__.update(self.rules)
-        return item
+        if not self.query.matches(item):
+            return item
+
+        func_transformer = get_transformer_for(item)
+        transformed_item = func_transformer(item, self.rules)
+        return transformed_item
--- tests/test_q.py
+++ tests/test_q.py
@@ -1,25 +1,29 @@
+from collections import namedtuple
+
 from lilq import Q, If
 
 from unittest.mock import Mock
 
+Item = namedtuple("Item", ["aaa", "bbb"])
+
 
 def test_simple_query():
-    q = Q(foo="bar")
-    item1 = Mock(foo="bar")
-    item2 = Mock(foo="quux")
+    q = Q(aaa="foo")
+    item1 = Mock(aaa="foo")
+    item2 = Mock(aaa="quux")
     assert q.matches(item1)
     assert not q.matches(item2)
 
 
 def test_simple_transformation():
     q = If(aaa="foo").then(aaa="bar", bbb="quux")
-    item1 = Mock(aaa="foo")
-    item2 = Mock(aaa="lol")
+    item1 = Item(aaa="foo", bbb=None)
+    item2 = Item(aaa="lol", bbb=None)
 
-    q.apply(item1)
-    q.apply(item2)
+    item1_new = q.apply(item1)
+    item2_new = q.apply(item2)
 
-    assert item1.aaa == "bar"
-    assert item1.bbb == "quux"
-    assert item2.aaa == "lol"
-    assert not hasattr(item2, "bbb") or item2.bbb != "quux"
+    assert item1_new.aaa == "bar"
+    assert item1_new.bbb == "quux"
+    assert item2_new.aaa == "lol"
+    assert item2_new.bbb is None
